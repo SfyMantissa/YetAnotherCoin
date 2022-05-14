a14bf0bac0a3b5143014bd289d0fea1ada660058
Sfy Mantissa <sfymantissa@protonmail.com>
2022-05-14 16:41:44 +0300

    Set default network to Rinkeby.

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,6 +25,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
   networks: {
     rinkeby: {
       url: process.env.ALCHEMY_KEY,
