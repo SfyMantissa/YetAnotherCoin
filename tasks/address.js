57b851c5148560fcb9adc74a882bd2b1763d4e39
Sfy Mantissa <sfymantissa@protonmail.com>
2022-05-14 16:42:04 +0300

    Updated the deployed contract Rinkeby address.

--- tasks/address.js
+++ tasks/address.js
@@ -1,2 +1,2 @@
-const yetAnotherCoinAddress = '0x18197ecA4A17fE63A7e8c7F3479e74ba7221CD16';
+const yetAnotherCoinAddress = '0xE88F855fADD8e8A30DeDF1E8dddCf4138a2D1325';
 module.exports = yetAnotherCoinAddress;
